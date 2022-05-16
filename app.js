026299dbda67bf2fa05154b0bf7f5af46e3d3a9c
pharadornb <B6134228@g.sut.ac.th>
2022-05-17 00:02:42 +0700

    Static page connect by express

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const debug = require('debug')('app');
 const morgan = require('morgan')
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
     res.send('Hello World');
