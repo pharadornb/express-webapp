6604cb426dcbad39393dde35302b35ab3a8a9f7b
pharadornb <B6134228@g.sut.ac.th>
2022-05-18 00:47:31 +0700

    course update 18/05/2565 00.46

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 const path = require('path');
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send('Hello World');
+    // res.send('Hello World wow');
+    res.render('index', {username: 'BomaPD', customers: ["bom", "boonruam", "eieiei"]});
 })
 
 app.listen(port, () => {
